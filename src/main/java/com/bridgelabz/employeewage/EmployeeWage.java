a2c86c0e96368e19057c32b62910624f99ddd28f
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-12 18:33:56 +0530

    Added: Wage of employees using switch case

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,16 +1,27 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWage {
+    public static final int  IS_PARTTIME = 1;
+    public static final int IS_FULLTIME = 2;
+    public static final int FULL_HOURS = 12;
+    public static final int PART_HOURS = 8;
+    public static final int WAGE_PER_HOUR = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int att = (int)(Math.random()*10) % 3;
-        int IS_PARTTIME = 1 , IS_FULLTIME = 2 , FULL_HOURS = 12 , PART_HOURS = 8 , WAGE_PER_HOUR = 20;
-        if(att== IS_FULLTIME)
-            System.out.println("Total Wage for Full Time is " + (FULL_HOURS * WAGE_PER_HOUR));
-        else
-            if (att == IS_PARTTIME)
-                System.out.println("Total Wage for Part Time is " + (PART_HOURS * WAGE_PER_HOUR));
-            else
+        int wage;
+
+        switch(att) {
+            case IS_FULLTIME :
+                wage = FULL_HOURS * WAGE_PER_HOUR;
+                System.out.println("Total Wage for Full Time is " + wage);
+                break;
+            case IS_PARTTIME :
+                wage = PART_HOURS * WAGE_PER_HOUR;
+                System.out.println("Total Wage for Part Time is " + wage);
+                break;
+            default:
                 System.out.println("Employee is Absent");
+        }
     }
 }
