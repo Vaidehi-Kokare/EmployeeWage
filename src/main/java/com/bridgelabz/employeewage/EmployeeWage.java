b9026bdb5bf7bd98ff89a562de04d07df124c674
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-12 17:20:16 +0530

    Added: Wage of full-time employee

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,5 +3,13 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int att = (int)(Math.random()*10) % 2;
+        int is_present = 1;
+        int hours = 8;
+        int wage_per_hour = 20;
+        if(att==is_present)
+            System.out.println("Total Wage for Full Time is " + (hours * wage_per_hour) + " hours");
+        else
+            System.out.println("Employee is Absent");
     }
 }
