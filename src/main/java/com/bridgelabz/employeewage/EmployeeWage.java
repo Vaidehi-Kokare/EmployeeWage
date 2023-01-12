089ed59cd8589a1fd008b893a4a3b85b8e7a857a
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-12 18:57:00 +0530

    Added: Monthly Wage of Employee

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -6,22 +6,25 @@ public class EmployeeWage {
     public static final int FULL_HOURS = 12;
     public static final int PART_HOURS = 8;
     public static final int WAGE_PER_HOUR = 20;
+    public static final int WORKING_DAYS_PER_MONTH = 20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int att = (int)(Math.random()*10) % 3;
-        int wage;
-
-        switch(att) {
-            case IS_FULLTIME :
-                wage = FULL_HOURS * WAGE_PER_HOUR;
-                System.out.println("Total Wage for Full Time is " + wage);
-                break;
-            case IS_PARTTIME :
-                wage = PART_HOURS * WAGE_PER_HOUR;
-                System.out.println("Total Wage for Part Time is " + wage);
-                break;
-            default:
-                System.out.println("Employee is Absent");
+        int att = (int) (Math.random() * 10) % 3;
+        int wage=0,total_wage=0;
+        for (int i = 1; i<=WAGE_PER_HOUR; i++) {
+            switch (att) {
+                case IS_FULLTIME:
+                    wage = FULL_HOURS * WAGE_PER_HOUR;
+                    System.out.println("Total Wage for Full Time is " + wage);
+                    break;
+                case IS_PARTTIME:
+                    wage = PART_HOURS * WAGE_PER_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            total_wage += wage;
         }
+        System.out.println("Monthly Wage is " + total_wage);
     }
 }
