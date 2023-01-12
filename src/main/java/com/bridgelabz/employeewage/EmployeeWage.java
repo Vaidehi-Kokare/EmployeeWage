69ea57d688469eb24f1bc1208015330afb702c66
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-01-12 18:08:54 +0530

    Added: Wage of full-time and part-time employees

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,13 +3,14 @@ package com.bridgelabz.employeewage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int att = (int)(Math.random()*10) % 2;
-        int is_present = 1;
-        int hours = 8;
-        int wage_per_hour = 20;
-        if(att==is_present)
-            System.out.println("Total Wage for Full Time is " + (hours * wage_per_hour) + " hours");
+        int att = (int)(Math.random()*10) % 3;
+        int IS_PARTTIME = 1 , IS_FULLTIME = 2 , FULL_HOURS = 12 , PART_HOURS = 8 , WAGE_PER_HOUR = 20;
+        if(att== IS_FULLTIME)
+            System.out.println("Total Wage for Full Time is " + (FULL_HOURS * WAGE_PER_HOUR));
         else
-            System.out.println("Employee is Absent");
+            if (att == IS_PARTTIME)
+                System.out.println("Total Wage for Part Time is " + (PART_HOURS * WAGE_PER_HOUR));
+            else
+                System.out.println("Employee is Absent");
     }
 }
